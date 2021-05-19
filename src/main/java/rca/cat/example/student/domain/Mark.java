ad7f8f77ae36de7c0ce8104173632607756ccdda
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-05-19 11:25:49 +0200

    missing info in Mark filled

--- src/main/java/rca/cat/example/student/domain/Mark.java
+++ src/main/java/rca/cat/example/student/domain/Mark.java
@@ -18,7 +18,23 @@ public class Mark {
 
     //All-args-id constructor
 
+    public Mark(Student student, Course course, Float score, Float maximum) {
+        this.student = student;
+        this.course = course;
+        this.score = score;
+        this.maximum = maximum;
+    }
+
     //All args constructor
+
+
+    public Mark(Long id, Student student, Course course, Float score, Float maximum) {
+        this.id = id;
+        this.student = student;
+        this.course = course;
+        this.score = score;
+        this.maximum = maximum;
+    }
 
     //Properties
     @Id
@@ -37,4 +53,44 @@ public class Mark {
 
     //Getters and Setters
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Float getScore() {
+        return score;
+    }
+
+    public void setScore(Float score) {
+        this.score = score;
+    }
+
+    public Float getMaximum() {
+        return maximum;
+    }
+
+    public void setMaximum(Float maximum) {
+        this.maximum = maximum;
+    }
 }
